3635fce15c6e9b8017673287b72543f6f0b02acf
Alone <tranluong460@gmail.com>
2024-05-20 10:00:08 +0700

    event and method api app

--- api-electron/app.js
+++ api-electron/app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, dialog } = require("electron/main");
+const { app, BrowserWindow, dialog } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,16 +7,43 @@ function createWindow() {
   });
 
   win.loadFile("index.html");
+
+  win.on("blur", () => {
+    console.log("Xay ra khi cua so trinh duyet mat tieu diem.");
+  });
+
+  win.on("focus", () => {
+    console.log("Xay ra khi cua so trinh duyet co tieu diem.");
+  });
 }
 
 app.on("will-finish-launching", () => {
-  console.log("Will finish launching...");
+  console.log("Xay ra khi ung dung chuan bi hoan tat viec khoi dong.");
 });
 
 app.whenReady().then(() => {
+  console.log(
+    "Xay ra khi Electron hoan tat viec khoi dong va san sang tao cua so trinh duyet."
+  );
+
+  const version = app.getVersion();
+  const locale = app.getLocale();
+  const country = app.getLocaleCountryCode();
+  const systemLocale = app.getSystemLocale();
+  const preferredSystemLanguages = app.getPreferredSystemLanguages();
+
+  console.log({
+    version,
+    locale,
+    country,
+    systemLocale,
+    preferredSystemLanguages,
+  });
+
   createWindow();
 
   app.on("activate", () => {
+    console.log("Xay ra khi ung dung duoc kich hoat.");
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -24,18 +51,19 @@ app.whenReady().then(() => {
 });
 
 app.on("before-quit", (event) => {
-  console.log("Before quit...");
+  console.log("Xay ra truoc khi ung dung thoat.");
 });
 
 app.on("will-quit", (event) => {
-  console.log("Will quit...");
+  console.log("Xay ra khi ung dung sap thoat.");
 });
 
 app.on("quit", (event) => {
-  console.log("Quit...");
+  console.log("Xay ra khi ung dung da thoat.");
 });
 
 app.on("window-all-closed", () => {
+  console.log("Xay ra khi tat ca cac cua so cua ung dung da dong.");
   if (process.platform !== "darwin") {
     app.quit();
   }
